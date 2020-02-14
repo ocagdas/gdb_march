351519e9ad9dba89acf5dcfc69358a5760b6df96
h.ozgur cagdas <ocagdas@zmartsolutions.com>
2020-02-14 15:09:59 +0000

    first cut

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gdb_march
-ubuntu based gdb_multiarch
+# gdb_march_ubuntu_18.04
+gdb_multiarch environment based on ubuntu 18.04
+
+# docker build
+```
+docker build -f ./DockerfileGdbMultiarchUbuntu18_04 -t zmart/gdb_march_ubuntu_18.04 .
+```
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+ 
+# If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 1000 as default
+CURRENT_UID=${uid:-1000}
+ 
+# Notify user about the UID selected
+echo "Current UID : $CURRENT_UID"
+# Create user called "docker" with selected UID
+useradd -d /home/docker --shell /bin/bash -u $CURRENT_UID -o -c "" -m docker
+passwd -d docker
+# Set "HOME" ENV variable for user's home directory
+export HOME=/home/docker
+echo "docker  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers; echo "Defaults    !requiretty" >> /etc/sudoers
+ 
+# Execute process
+exec /usr/local/bin/gosu docker "$@"
