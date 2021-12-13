88184fbb99e8cb663dda862fd87b77ea27c1d60e
ocagdas <ocagdas@gmail.com>
2021-12-13 15:22:35 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,9 @@ passwd -d docker
 # Set "HOME" ENV variable for user's home directory
 export HOME=/home/docker
 echo "docker  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers; echo "Defaults    !requiretty" >> /etc/sudoers
- 
+
+echo "alias grep='grep -a'" >> /home/docker/.bashrc
+
 # Execute process
 #exec /usr/local/bin/gosu docker "$@"
 exec gosu docker "$@"
