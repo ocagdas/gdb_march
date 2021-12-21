efc70bb3522e3d5cf742660f20eccf6b36e96921
h.ozgur cagdas <ocagdas@zmartsolutions.com>
2021-12-21 22:45:39 +0000

    reverted the grep changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ passwd -d docker
 export HOME=/home/docker
 echo "docker  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers; echo "Defaults    !requiretty" >> /etc/sudoers
 
-echo "alias grep='grep -a'" >> /home/docker/.bashrc
+#echo "alias grep='grep -a'" >> /home/docker/.bashrc
 
 # Execute process
 #exec /usr/local/bin/gosu docker "$@"
